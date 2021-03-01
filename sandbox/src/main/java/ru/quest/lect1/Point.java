dda5d1845f2a8589fa50d22ef20ba1c87d2ea863
DanteAut2 <yaroslav.parhim2@gmail.com>
2021-03-01 16:39:29 +0300

    Я создал класс Поинт, в котором объявил 2 новые точки

---
+++ sandbox/src/main/java/ru/quest/lect1/Point.java
@@ -0,0 +1,13 @@
+package ru.quest.lect1;
+
+public class Point {
+    public Point(int x, int y) {
+    }
+
+    public static void main (String[] args) {
+        Point a = new Point(2,4);
+        Point b = new Point(3,2);
+    }
+}
+
+
--- sandbox/src/main/java/ru/quest/lect1/questOne.java
+++ sandbox/src/main/java/ru/quest/lect1/questOne.java
@@ -1,3 +1,5 @@
+package ru.quest.lect1;
+
 public class questOne {
 	public static void main(String[] args){
 		System.out.println("Inputed some text for first question");
