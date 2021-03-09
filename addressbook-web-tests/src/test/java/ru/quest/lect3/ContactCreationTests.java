a7c5c9196783cf53d62dd71eb72746bef7984180
DanteAut2 <yaroslav.parhim2@gmail.com>
2021-03-09 22:04:26 +0300

    Создал DeleteContact, отредачил ContactCreationTests

--- addressbook-web-tests/src/test/java/ru/quest/lect3/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/quest/lect3/ContactCreationTests.java
@@ -6,24 +6,10 @@ import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import ru.quest.lect3.tests.TestBase;
 
-public class ContactCreationTests {
-    private WebDriver wd;
+public class ContactCreationTests extends TestBase {
 
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/edit.php");
-        login("admin", "secret");
-    }
-    private void login(String login, String password) { //Логин на сайте
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys(login);
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys(password);
-        wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
     @Test
     public void creatingNewUser() throws Exception { //Главный список действий
         createNewUser();
@@ -40,126 +26,5 @@ public class ContactCreationTests {
         confirm();
         homepage();
         logout();
-    }
-    private void logout() {
-        wd.findElement(By.linkText("Logout")).click();
-    }
-    private void homepage() {
-        wd.findElement(By.linkText("home page")).click();
-    }
-    private void confirm() {
-        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-    }
-    private void InputSecondary(InputSecondarys inputSecondarys) { //Ввод полей Secondary
-        wd.findElement(By.name("address2")).click();
-        wd.findElement(By.name("address2")).clear();
-        wd.findElement(By.name("address2")).sendKeys(inputSecondarys.getSecondAddress());
-        wd.findElement(By.name("phone2")).click();
-        wd.findElement(By.name("phone2")).clear();
-        wd.findElement(By.name("phone2")).sendKeys(inputSecondarys.getSecondAddressHomeNumber());
-        wd.findElement(By.name("notes")).clear();
-        wd.findElement(By.name("notes")).sendKeys(inputSecondarys.getNotes());
-    }
-    private void inputBdayAday(InputDates inputDates) { //Ввод Дат
-        wd.findElement(By.name("bday")).click();
-        new Select(wd.findElement(By.name("bday"))).selectByVisibleText(inputDates.getbDayDay());
-        wd.findElement(By.name("bday")).click();
-        wd.findElement(By.name("bmonth")).click();
-        new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(inputDates.getbDayMonth());
-        wd.findElement(By.name("bmonth")).click();
-        wd.findElement(By.name("byear")).click();
-        wd.findElement(By.name("byear")).clear();
-        wd.findElement(By.name("byear")).sendKeys(inputDates.getbDayYear());
-        wd.findElement(By.name("aday")).click();
-        new Select(wd.findElement(By.name("aday"))).selectByVisibleText(inputDates.getaDayDay());
-        wd.findElement(By.name("aday")).click();
-        wd.findElement(By.name("amonth")).click();
-        new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(inputDates.getaDayMonth());
-        wd.findElement(By.name("amonth")).click();
-        wd.findElement(By.name("ayear")).click();
-        wd.findElement(By.name("ayear")).clear();
-        wd.findElement(By.name("ayear")).sendKeys(inputDates.getaDayYear());
-    }
-    private void inputHomepage(String homepage) {
-        wd.findElement(By.name("homepage")).click();
-        wd.findElement(By.name("homepage")).clear();
-        wd.findElement(By.name("homepage")).sendKeys(homepage);
-    }
-    private void inputEmails(InputEmails inputEmails) {
-        wd.findElement(By.name("email")).click();
-        wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(inputEmails.getFirstEmail());
-        wd.findElement(By.name("email2")).clear();
-        wd.findElement(By.name("email2")).sendKeys(inputEmails.getSecondEmail());
-        wd.findElement(By.name("email3")).clear();
-        wd.findElement(By.name("email3")).sendKeys(inputEmails.getThirdEmail());
-    }
-    private void homeMobileWorkFaxNumber(InputNumbers inputNumbers) { //Ввод ввод номеров телефонов
-        wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys(inputNumbers.getHomeNumber());
-        wd.findElement(By.name("mobile")).click();
-        wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys(inputNumbers.getMobileNumber());
-        wd.findElement(By.name("work")).click();
-        wd.findElement(By.name("work")).clear();
-        wd.findElement(By.name("work")).sendKeys(inputNumbers.getWorkNumber());
-        wd.findElement(By.name("fax")).click();
-        wd.findElement(By.name("fax")).clear();
-        wd.findElement(By.name("fax")).sendKeys(inputNumbers.getFaxNumber());
-    }
-    private void inputAddress(String address) {
-        wd.findElement(By.name("address")).click();
-        wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(address);
-    }
-    private void inputCompany(String company) {
-        wd.findElement(By.name("company")).click();
-        wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys(company);
-    }
-    private void inputTitle(String title) {
-        wd.findElement(By.name("title")).click();
-        wd.findElement(By.name("title")).clear();
-        wd.findElement(By.name("title")).sendKeys(title);
-    }
-    private void inputNickname(String nickname) {
-        wd.findElement(By.name("nickname")).click();
-        wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys(nickname);
-    }
-    private void inputFirstMiddleLastName(InputNames inputNames) { //Ввод ФИО
-        wd.findElement(By.name("firstname")).click();
-        wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(inputNames.getFirstName());
-        wd.findElement(By.name("middlename")).click();
-        wd.findElement(By.name("middlename")).clear();
-        wd.findElement(By.name("middlename")).sendKeys(inputNames.getMiddleName());
-        wd.findElement(By.name("lastname")).click();
-        wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(inputNames.getThirdName());
-    }
-    private void createNewUser() {
-        wd.findElement(By.linkText("add new")).click();
-    }
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() throws Exception {
-        wd.quit();
-    }
-    private boolean isElementPresent(By by) {
-        try {
-            wd.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-    private boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
     }
 }
