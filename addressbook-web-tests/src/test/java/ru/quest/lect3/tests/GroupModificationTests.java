fd2fef10f378027135835aaf2e749b56c00306a9
DanteAut2 <yaroslav.parhim2@gmail.com>
2021-03-11 20:18:59 +0300

    выполнил видео 2.9

---
+++ addressbook-web-tests/src/test/java/ru/quest/lect3/tests/GroupModificationTests.java
@@ -0,0 +1,18 @@
+package ru.quest.lect3.tests;
+
+import org.testng.annotations.Test;
+import ru.quest.lect3.appmanager.TestBase;
+import ru.quest.lect3.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification () {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
