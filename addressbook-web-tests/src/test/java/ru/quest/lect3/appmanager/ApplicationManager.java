f44c6aa6c5370c6253f1146857c9a08accabc150
DanteAut2 <yaroslav.parhim2@gmail.com>
2021-03-16 11:09:16 +0300

    Исправил задание 7

--- addressbook-web-tests/src/test/java/ru/quest/lect3/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/quest/lect3/appmanager/ApplicationManager.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import sun.plugin2.util.BrowserType;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,19 +17,19 @@ public class ApplicationManager {
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     public ContactHelper contactHelper;
-    private int browser;
+    private String browser;
 
-    public ApplicationManager(int browser) {
+    public ApplicationManager(String browser) {
         this.browser = browser;
     }
 
 
     public void init() {
-        if (browser == BrowserType.MOZILLA) {
+        if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
-        } else if (browser == BrowserType.DEFAULT) {
+        } else if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
-        } else if (browser == BrowserType.INTERNET_EXPLORER) {
+        } else if (browser.equals(BrowserType.IE)) {
             wd = new InternetExplorerDriver();
         }
 
--- addressbook-web-tests/src/test/java/ru/quest/lect3/appmanager/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/quest/lect3/appmanager/TestBase.java
@@ -1,22 +1,13 @@
 package ru.quest.lect3.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import ru.quest.lect3.appmanager.ApplicationManager;
-import ru.quest.lect3.model.*;
-import sun.plugin2.util.BrowserType;
 
-import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.DEFAULT);
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
