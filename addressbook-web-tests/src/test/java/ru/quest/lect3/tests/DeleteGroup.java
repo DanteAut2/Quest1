4b6b0e0d83e87afafaeea142c0e02c473dae2bdd
DanteAut2 <yaroslav.parhim2@gmail.com>
2021-03-16 19:05:23 +0300

    Прошел видео 3.10

--- addressbook-web-tests/src/test/java/ru/quest/lect3/tests/DeleteGroup.java
+++ addressbook-web-tests/src/test/java/ru/quest/lect3/tests/DeleteGroup.java
@@ -2,12 +2,16 @@ package ru.quest.lect3.tests;
 
 import org.testng.annotations.Test;
 import ru.quest.lect3.appmanager.TestBase;
+import ru.quest.lect3.model.GroupData;
 
 public class DeleteGroup extends TestBase {
 
     @Test
     public void testDeleteGroup() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
