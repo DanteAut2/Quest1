ac444befecc34cc5a8cd1d7092e5f95ce73754d9
DanteAut2 <yaroslav.parhim2@gmail.com>
2021-03-23 12:17:33 +0300

    Прошел видео 4.7

--- addressbook-web-tests/src/test/java/ru/quest/lect3/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/quest/lect3/tests/GroupModificationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.quest.lect3.appmanager.TestBase;
 import ru.quest.lect3.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -20,11 +21,15 @@ public class GroupModificationTests extends TestBase {
 
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test1", null, null);
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
 
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
