5958f92490ebcccba49226339d87b49203066150
DanteAut2 <yaroslav.parhim2@gmail.com>
2021-03-29 16:53:06 +0300

    Прошел видео 5.3

--- addressbook-web-tests/src/test/java/ru/quest/lect3/tests/DeleteGroup.java
+++ addressbook-web-tests/src/test/java/ru/quest/lect3/tests/DeleteGroup.java
@@ -13,28 +13,28 @@ public class DeleteGroup extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
-        app.getNavigationHelper().gotoGroupPage();
-        if (! app.getGroupHelper().isThereAGroup()) {
-            app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        app.goTo().groupPage();
+        if (app.group().list().size() == 0) {
+            app.group().create(new GroupData("test1", "test2", "test3"));
         }
     }
 
     @Test
     public void testDeleteGroup() throws Exception {
 
-        List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(before.size() - 1);
-        app.getGroupHelper().deleteSelectedGroups();
-        app.getGroupHelper().returnToGroupPage();
-        List<GroupData> after = app.getGroupHelper().getGroupList();
+        List<GroupData> before = app.group().list();
+        int index = before.size() - 1;
+        app.group().delete(index);
+        List<GroupData> after = app.group().list();
         Assert.assertEquals(after.size(), before.size() - 1);
 
-        before.remove(before.size() - 1);
+        before.remove(index);
         Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
         before.sort(byId);
         after.sort(byId);
         Assert.assertEquals(before, after);
 
     }
+
 
 }
