a829dda2625bcf8320154ba3d3bf64fc66d56ad4
DanteAut2 <yaroslav.parhim2@gmail.com>
2021-04-01 12:37:02 +0300

    Починил тесты под 5.7

--- addressbook-web-tests/src/test/java/ru/quest/lect3/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/quest/lect3/tests/ContactCreationTests.java
@@ -16,9 +16,7 @@ public class ContactCreationTests extends TestBase {
         Contacts before = app.contact().all();
         app.contact().createNewUser();
         ContactData contact = new ContactData().withFirstName("dsfsd").withMiddleName("gfdsfsd").withThirdName("fsdfsdd").withNickname("dsgdsg").withTitle("gsdfghsd").withCompany("sdfgsdf").withAddress("fsdfs").withFirstEmail("gfdsf").withSecondEmail("gdsjkfs").withThirdEmail("fjsdfj").withbDayDay("11").withbDayMonth("May").withbDayYear("1996").withaDayDay("12").withaDayMonth("June").withaDayYear("1994").withHomeNumber("45345").withMobileNumber("5436534").withWorkNumber("63463").withFaxNumber("345734").withSecondAddress("hfshs").withSecondAddressHomeNumber("6435634").withNotes("gdgsd").withHomepage("gdsdfgs").withGroup("test1");
-        app.contact().inputAllContactInfo(contact, true);
-        app.contact().confirm();
-        app.contact().homepage();
+        app.contact().create(contact, true);
         Contacts after = app.contact().all();
         assertThat(after.size(), equalTo(before.size() + 1));
 
