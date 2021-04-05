293c66fda1169493f4559a638b3e10f964e9d5fc
DanteAut2 <yaroslav.parhim2@gmail.com>
2021-04-05 19:19:06 +0300

    Выполнил видео 6.2 для групп и контактов

---
+++ addressbook-web-tests/src/test/java/ru/quest/lect3/generators/ContactDataGenerator.java
@@ -0,0 +1,36 @@
+package ru.quest.lect3.generators;
+
+import ru.quest.lect3.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactDataGenerator {
+    public static void main (String[] args) throws IOException {
+        int count = Integer.parseInt(args[0]);
+        File file = new File(args[1]);
+
+        List<ContactData> contacts = generateContacts(count);
+        save(contacts, file);
+    }
+
+    private static void save(List<ContactData> contacts, File file) throws IOException {
+        Writer writer = new FileWriter(file);
+        for (ContactData contact : contacts) {
+            writer.write(String.format("%s;%s;%s\n", contact.getFirstName(), contact.getMiddleName(), contact.getThirdName()));
+        }
+        writer.close();
+    }
+
+    private static List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        for (int i = 0; i < count; i++) {
+            contacts.add(new ContactData().withFirstName(String.format("firstname %s", i)).withMiddleName(String.format("middlename %s", i)).withLastName(String.format("lastname %s", i)));
+        }
+        return contacts;
+    }
+}
