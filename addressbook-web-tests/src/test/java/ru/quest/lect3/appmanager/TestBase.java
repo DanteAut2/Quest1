be8696b91ae184865fbeb64811b924d6ad499f3f
DanteAut2 <yaroslav.parhim2@gmail.com>
2021-04-13 02:32:58 +0300

    Сделал пропущенный 6.11

--- addressbook-web-tests/src/test/java/ru/quest/lect3/appmanager/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/quest/lect3/appmanager/TestBase.java
@@ -1,11 +1,20 @@
 package ru.quest.lect3.appmanager;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import ru.quest.lect3.tests.GroupCreationTests;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 
 public class TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static ApplicationManager app;
 
@@ -18,5 +27,15 @@ public class TestBase {
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
     }
 }
--- addressbook-web-tests/src/test/java/ru/quest/lect3/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/quest/lect3/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.quest.lect3.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.quest.lect3.appmanager.TestBase;
@@ -21,6 +23,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
+
 
     @DataProvider
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
@@ -63,7 +67,6 @@ public class GroupCreationTests extends TestBase {
         Groups after = app.group().all();
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-
     }
 
     @Test
