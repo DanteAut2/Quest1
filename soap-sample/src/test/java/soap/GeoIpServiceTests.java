a9736188d7d9a5174bae66b178a951c0bd296230
DanteAut2 <yaroslav.parhim2@gmail.com>
2021-04-26 18:43:00 +0300

    9.2 ну почти

--- soap-sample/src/test/java/soap/GeoIpServiceTests.java
+++ soap-sample/src/test/java/soap/GeoIpServiceTests.java
@@ -1,16 +1,18 @@
 package soap;
 
 import com.lavasoft.GeoIPService;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.nio.charset.StandardCharsets;
 
 import static org.testng.Assert.assertEquals;
 
 public class GeoIpServiceTests {
 
     @Test
-    public void testMyIp() {
+    public void testGeoIpService() {
         String ipLocation20 = new GeoIPService().getGeoIPServiceSoap12().getIpLocation20("109.252.201.163");
-        assertEquals(ipLocation20.ge);
+        assertEquals(ipLocation20.getBytes(StandardCharsets.UTF_8), "RU");
     }
+
 }
