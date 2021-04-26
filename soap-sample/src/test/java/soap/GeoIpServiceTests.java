8861b49087352fd0e584be70f8f918ddc2c71b7b
DanteAut2 <yaroslav.parhim2@gmail.com>
2021-04-26 18:50:47 +0300

    9.2 сделал

--- soap-sample/src/test/java/soap/GeoIpServiceTests.java
+++ soap-sample/src/test/java/soap/GeoIpServiceTests.java
@@ -3,16 +3,17 @@ package soap;
 import com.lavasoft.GeoIPService;
 import org.testng.annotations.Test;
 
-import java.nio.charset.StandardCharsets;
-
 import static org.testng.Assert.assertEquals;
 
 public class GeoIpServiceTests {
 
     @Test
     public void testGeoIpService() {
-        String ipLocation20 = new GeoIPService().getGeoIPServiceSoap12().getIpLocation20("109.252.201.163");
-        assertEquals(ipLocation20.getBytes(StandardCharsets.UTF_8), "RU");
+        String ipLocation20 = new GeoIPService().getGeoIPServiceSoap12().getCountryISO2ByName("USA");
+        assertEquals(ipLocation20, "<GeoIP><Country>US</Country></GeoIP>");
     }
+
+    //String isoCountryName = new GeoIPService().getGeoIPServiceSoap12().getCountryISO2ByName("USA");
+    //assertEquals(isoCountryName, "<GeoIP><Country>US</Country></GeoIP>");
 
 }
